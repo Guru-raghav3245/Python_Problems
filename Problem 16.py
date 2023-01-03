9e0b2de0b56671404aad501970ef4b2f51c9119d
Guru-raghav3245 <master.guru.raghav@gmail.com>
2023-01-03 14:00:23 +0530

    Solved problem 16

---
+++ Problem 16.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+password = []
+for i in range(5):
+    string.ascii_letters
+    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    password.append(random.choice(string.ascii_letters))
+    password.append(random.randint(1, 9))
+random.shuffle(password)
+print(password)
